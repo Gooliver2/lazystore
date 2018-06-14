db1634168d8d92d86a92c9cbda799d6205e9b992
d.choban <dmytro.choban@sanoma.com>
2018-06-14 09:21:51 +0300

    added completing bucket on transaction creating

--- transaction/serializers.py
+++ transaction/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from bucket.models import Bucket
 from transaction.models import Transaction
 
 
@@ -14,3 +15,10 @@ class TransactionCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
+
+    def create(self, validated_data):
+        instance = super().create(validated_data)
+        instance.bucket.status = Bucket.COMPLETED
+        instance.bucket.save()
+        return instance
+
